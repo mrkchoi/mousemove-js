f3a0d03d3f1f82a6da5399dd746832c9811385b6
Kenny <kennethichoi@gmail.com>
2018-11-08 23:45:48 -0800

    Added background color change on mousemove

--- scripts/app.js
+++ scripts/app.js
@@ -2,6 +2,7 @@
 let container = document.querySelector('.container-full');
 let text = document.querySelector('.header');
 let walk = 10;
+let walk2 = 1000;
 
 
 
@@ -19,12 +20,17 @@ function changeShadow(e) {
     
     const xWalk = Math.round((x / containerWidth * walk) - (walk / 2));
     const yWalk = Math.round((y / containerHeight * walk) - (walk / 2));
+    const xWalk2 = Math.round((x / containerWidth * walk2) - (walk2 / 2));
+    const yWalk2 = Math.round((y / containerHeight * walk2) - (walk2 / 2));
 
     text.style.textShadow = `
-    ${xWalk}px ${yWalk}px 0 rgba(255,0,0,.8),
-    ${xWalk * -1}px ${yWalk}px 0 rgba(0,255,0,.8),
-    ${yWalk}px ${xWalk}px 0 rgba(0,0,255,.8),
-    ${yWalk * -1}px ${xWalk}px 0 rgba(255,255,0,.8)
+        ${xWalk}px ${yWalk}px 0 rgba(255,0,0,.8),
+        ${xWalk * -1}px ${yWalk}px 0 rgba(0,255,0,.8),
+        ${yWalk}px ${xWalk}px 0 rgba(0,0,255,.8),
+        ${yWalk * -1}px ${xWalk}px 0 rgba(255,255,0,.8)
+    `;
+    container.style.backgroundColor = `
+        rgba(${xWalk2 * -1},${yWalk2 * -1},${yWalk2}, .2)
     `;
 
     console.log(xWalk,yWalk);
