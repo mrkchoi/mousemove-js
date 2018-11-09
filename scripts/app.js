0eca5a75d4aef7ecfce71733016e70ffc22608f3
Kenny <kennethichoi@gmail.com>
2018-11-08 23:39:59 -0800

    Completed v1

---
+++ scripts/app.js
@@ -0,0 +1,34 @@
+// Set DOM variables
+let container = document.querySelector('.container-full');
+let text = document.querySelector('.header');
+let walk = 10;
+
+
+
+// Set functions
+function changeShadow(e) {
+    let containerWidth = container.offsetWidth;
+    let containerHeight = container.offsetHeight;
+    let x = e.offsetX; 
+    let y = e.offsetY;
+    
+    if(this !== e.target) {
+        x = x + e.target.offsetLeft;
+        y = y + e.target.offsetTop;
+    }
+    
+    const xWalk = Math.round((x / containerWidth * walk) - (walk / 2));
+    const yWalk = Math.round((y / containerHeight * walk) - (walk / 2));
+
+    text.style.textShadow = `
+    ${xWalk}px ${yWalk}px 0 rgba(255,0,0,.8),
+    ${xWalk * -1}px ${yWalk}px 0 rgba(0,255,0,.8),
+    ${yWalk}px ${xWalk}px 0 rgba(0,0,255,.8),
+    ${yWalk * -1}px ${xWalk}px 0 rgba(255,255,0,.8)
+    `;
+
+    console.log(xWalk,yWalk);
+}
+
+// Set event listeners
+container.addEventListener('mousemove', changeShadow);
